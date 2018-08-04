1e5c28de6ca9251bcfc7fb2afa47afdbdd91a295
Pavel Novoa <pavel.novoa@gmail.com>
2018-08-04 15:28:14 -0500

    aaaa

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@
  */
 public class App {
     public String getGreeting() {
-        return "Hello world!!!";
+        return "Hello world!!!222";
     }
 
     public static void main(String[] args) {
